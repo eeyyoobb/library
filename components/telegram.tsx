bce2def3d65f17c05d90ef3353789a66fd60d7b4
Yeselamsew Bekele <yesjobkb@gmail.com>
2026-08-18 09:43:53 +0300

    tele

--- components/telegram.tsx
+++ components/telegram.tsx
@@ -3,21 +3,34 @@
 import { useEffect, useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
-
-import { WebApp } from "@twa-dev/types";
+import WebApp from "@twa-dev/sdk";
+import { WebApp as TELEG } from "@twa-dev/types";
 
 declare global {
   interface Window {
     Telegram?: {
-      WebApp?: WebApp;
+      WebApp?: TELEG;
     };
   }
 }
-
+interface UserData {
+  id: number;
+  first_name: string;
+  last_name?: string;
+  username?: string;
+  language_code: string;
+  is_premium?: boolean;
+}
 export default function TelegramAuth() {
   const router = useRouter();
-
+  const [userData, setUserData] = useState<UserData | null>(null);
   const [checking, setChecking] = useState(true);
+
+  useEffect(() => {
+    if (WebApp.initDataUnsafe.user) {
+      setUserData(WebApp.initDataUnsafe.user as UserData);
+    }
+  }, []);
 
   useEffect(() => {
     async function authenticate() {
@@ -85,6 +98,21 @@ export default function TelegramAuth() {
       <div className="rounded-lg bg-black px-6 py-4 text-white">
         Signing in with Telegram...
       </div>
+      {userData ? (
+        <>
+          <h1 className="text-2xl font-bold mb-4">User Data</h1>
+          <ul>
+            <li>ID: {userData.id}</li>
+            <li>First Name: {userData.first_name}</li>
+            <li>Last Name: {userData.last_name || "N/A"}</li>
+            <li>Username: {userData.username || "N/A"}</li>
+            <li>Language Code: {userData.language_code}</li>
+            <li>Is Premium: {userData.is_premium ? "Yes" : "No"}</li>
+          </ul>
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 }
