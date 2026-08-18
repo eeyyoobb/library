5417c7715c73021ecd5cd74f3aa636ea64747257
Yeselamsew Bekele <yesjobkb@gmail.com>
2026-08-18 09:49:46 +0300

    fff

--- components/telegram.tsx
+++ components/telegram.tsx
@@ -23,14 +23,14 @@ interface UserData {
 }
 export default function TelegramAuth() {
   const router = useRouter();
-  // const [userData, setUserData] = useState<UserData | null>(null);
+  const [userData, setUserData] = useState<UserData | null>(null);
   const [checking, setChecking] = useState(true);
 
-  // useEffect(() => {
-  //   if (WebApp.initDataUnsafe.user) {
-  //     setUserData(WebApp.initDataUnsafe.user as UserData);
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (WebApp.initDataUnsafe.user) {
+      setUserData(WebApp.initDataUnsafe.user as UserData);
+    }
+  }, []);
 
   useEffect(() => {
     async function authenticate() {
@@ -96,23 +96,22 @@ export default function TelegramAuth() {
   return (
     <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/50">
       <div className="rounded-lg bg-black px-6 py-4 text-white">
-        Signing in with Telegram...
+        {userData ? (
+          <>
+            <h1 className="text-2xl font-bold mb-4">User Data</h1>
+            <ul>
+              <li>ID: {userData.id}</li>
+              <li>First Name: {userData.first_name}</li>
+              <li>Last Name: {userData.last_name || "N/A"}</li>
+              <li>Username: {userData.username || "N/A"}</li>
+              <li>Language Code: {userData.language_code}</li>
+              <li>Is Premium: {userData.is_premium ? "Yes" : "No"}</li>
+            </ul>
+          </>
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
-      {/* {userData ? (
-        <>
-          <h1 className="text-2xl font-bold mb-4">User Data</h1>
-          <ul>
-            <li>ID: {userData.id}</li>
-            <li>First Name: {userData.first_name}</li>
-            <li>Last Name: {userData.last_name || "N/A"}</li>
-            <li>Username: {userData.username || "N/A"}</li>
-            <li>Language Code: {userData.language_code}</li>
-            <li>Is Premium: {userData.is_premium ? "Yes" : "No"}</li>
-          </ul>
-        </>
-      ) : (
-        <div>Loading...</div>
-      )} */}
     </div>
   );
 }
